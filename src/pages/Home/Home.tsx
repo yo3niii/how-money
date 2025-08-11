f3cc434ed0897778d61defc2aa696503fd0b5f71
yo3niii <milkyd03@gmail.com>
2025-08-11 20:52:29 +0900

    feat: create, home page컴포넌트들 생성

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Calendar from "../../components/Home/Calendar/Calendar";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Calendar />
+    </div>
+  );
+}
+
+export default Home;
