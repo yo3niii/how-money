0e39a3018808bee3f63fa01ae43575c3ab34c656
yo3niii <milkyd03@gmail.com>
2025-08-11 22:59:01 +0900

    feat: Calendar의 style 수정

--- src/components/Home/Calendar/CalendarBody.tsx
+++ src/components/Home/Calendar/CalendarBody.tsx
@@ -2,6 +2,7 @@ function CalendarBody() {
   const today = new Date();
   const currentYear = today.getFullYear();
   const currentMonth = today.getMonth() + 1;
+  const currentDate = today.getDate();
 
   const firstDate = new Date(currentYear, currentMonth - 1, 1);
   const dayOfFirstDate = firstDate.getDay();
@@ -34,13 +35,34 @@ function CalendarBody() {
     weeks.push(dateInCal.slice(i, i + 7));
   }
 
+  function isToday(date: Date) {
+    if (
+      date.getFullYear() == currentYear &&
+      date.getMonth() + 1 == currentMonth &&
+      date.getDate() == currentDate
+    ) {
+      return true;
+    }
+    return false;
+  }
+
   return (
     <tbody>
       {weeks.map((week) => {
         return (
           <tr>
             {week.map((date) => {
-              return <td>{date.getDate()}</td>;
+              return (
+                <td
+                  style={{
+                    opacity: date.getMonth() + 1 == currentMonth ? 1 : 0.5,
+                    color: isToday(date) ? "hotpink" : "white",
+                    fontWeight: isToday(date) ? "bold" : "normal",
+                  }}
+                >
+                  {date.getDate()}
+                </td>
+              );
             })}
           </tr>
         );
