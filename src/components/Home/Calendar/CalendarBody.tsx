2f24f3c3d60376fcf588eee2d0d48a3d5d2e756e
yo3niii <milkyd03@gmail.com>
2025-08-26 21:51:56 +0900

    feat: workplace 테이블에서 불러온 직장정보와 일한 날짜,시간 을 workingtime 테이블에 저장

--- src/components/Home/Calendar/CalendarBody.tsx
+++ src/components/Home/Calendar/CalendarBody.tsx
@@ -1,3 +1,27 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useEffect, useState, type FormEvent } from "react";
+import type { SelectChangeEvent } from "@mui/material/Select";
+import supabase from "../../../utils/supabase/client";
+
+type Workplace = {
+  id: number;
+  name: string;
+  hourly_rate: number;
+  color: string;
+};
+
 function CalendarBody() {
   const today = new Date();
   const currentYear = today.getFullYear();
@@ -46,28 +70,140 @@ function CalendarBody() {
     return false;
   }
 
+  const [workplaceData, setWorkplaceData] = useState<Workplace[]>([]);
+
+  const [open, setOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedWorkplaceId, setSelectedWorkplaceId] = useState<string>("");
+
+  useEffect(() => {
+    const fetchWorkplaceDate = async () => {
+      const { data } = await supabase.from("workplace").select();
+
+      if (data) {
+        console.log(data);
+        setWorkplaceData(data);
+      }
+    };
+
+    fetchWorkplaceDate();
+  }, []);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    setSelectedWorkplaceId(event.target.value);
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const timeRaw = formData.get("time");
+
+    const time = Number(timeRaw);
+
+    if (!Number.isFinite(time)) {
+      console.error("Invalid form values", { time });
+      return;
+    }
+
+    const { error } = await supabase
+      .from("working_time")
+      .insert([
+        { workplace_id: selectedWorkplaceId, time, date: selectedDate },
+      ]);
+
+    if (error) {
+      console.error("Failed to insert workplace", error);
+      return;
+    }
+
+    form.reset();
+    setOpen(false);
+  };
+
   return (
-    <tbody>
-      {weeks.map((week) => {
-        return (
-          <tr>
-            {week.map((date) => {
-              return (
-                <td
-                  style={{
-                    opacity: date.getMonth() + 1 == currentMonth ? 1 : 0.5,
-                    color: isToday(date) ? "hotpink" : "white",
-                    fontWeight: isToday(date) ? "bold" : "normal",
-                  }}
-                >
-                  {date.getDate()}
-                </td>
-              );
-            })}
-          </tr>
-        );
-      })}
-    </tbody>
+    <>
+      <tbody>
+        {weeks.map((week, weekIdx) => {
+          return (
+            <tr key={`week-${weekIdx}`}>
+              {week.map((date) => {
+                return (
+                  <td
+                    key={`date-${date.toISOString()}`}
+                    className="table-date"
+                    style={{
+                      opacity: date.getMonth() + 1 == currentMonth ? 1 : 0.5,
+                      color: isToday(date) ? "hotpink" : "white",
+                      fontWeight: isToday(date) ? "bold" : "normal",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => {
+                      handleOpen();
+                      setSelectedDate(date);
+                    }}
+                  >
+                    {date.getDate()}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>일한 시간 등록</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {selectedDate.getFullYear()}.{selectedDate.getMonth() + 1}.
+            {selectedDate.getDate()}
+          </DialogContentText>
+          <br />
+          <form onSubmit={handleSubmit} id="subscription-form">
+            <FormControl fullWidth>
+              <InputLabel id="workplace-select-label">직장 정보</InputLabel>
+              <Select
+                labelId="workplace-select-label"
+                id="workplace-select"
+                label="Workplace"
+                value={selectedWorkplaceId}
+                onChange={handleChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                {workplaceData &&
+                  workplaceData.map((data) => (
+                    <MenuItem key={data.id} value={data.id.toString()}>
+                      {data.name}: {data.hourly_rate}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </FormControl>
+            <TextField
+              required
+              id="time"
+              name="time"
+              label="일한 시간(분)"
+              fullWidth
+              placeholder="ex) 120"
+              variant="standard"
+            />
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit" form="subscription-form">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
